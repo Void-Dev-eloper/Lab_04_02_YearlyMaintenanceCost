ceea18828a308585dc1bd7cf325432de202b0392
ZN-Zz <badhikari1211@gmail.com>
2024-01-31 10:59:59 -0500

    Finished

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+        double summerCost = 399.99;
+        double springCost = summerCost;
+        double fallCost = summerCost;
+        double winterCost = summerCost;
+        double yearlyCost = summerCost * 4;
+
+        System.out.println("Summer Cost: $" + summerCost);
+        System.out.println("Spring Cost: $" + springCost);
+        System.out.println("Fall Cost: $" + fallCost);
+        System.out.println("Winter Cost: $" + winterCost);
+        System.out.println("Yearly Cost: $" + yearlyCost);
+    }
+}
